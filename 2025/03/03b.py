86e45da5af635b470af2b306f5c69d3c9e5efec9
jameslaight <150948866+jameslaight@users.noreply.github.com>
2025-12-03 13:29:33 +0000

    solved day 03

---
+++ 2025/03/03b.py
@@ -0,0 +1,23 @@
+banks = [[int(c) for c in line] for line in open("2025/03/input.txt").read().splitlines()]
+
+def jolt(bank, batteries): #i knew it!!
+    batsAfter = batteries - 1
+
+    if batsAfter < 0:
+        return 0
+
+    best = -1
+    for i in range(len(bank) - batsAfter): #look for leftmost max value
+        battery = bank[i]
+
+        if best == -1 or battery > best:
+            best = battery
+            bestpos = i
+
+    return best * 10 ** batsAfter + jolt(bank[bestpos + 1:], batsAfter)
+
+total = 0
+for bank in banks:
+    total += jolt(bank, 12)
+
+print(total)
