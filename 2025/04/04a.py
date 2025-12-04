dfc375438c4585d743b1386e6a55cd4a8ee15a65
jameslaight <150948866+jameslaight@users.noreply.github.com>
2025-12-04 21:05:25 +0000

    solved day 04

---
+++ 2025/04/04a.py
@@ -0,0 +1,28 @@
+grid = [[c == '@' for c in line] for line in open("2025/04/input.txt").read().splitlines()]
+width = len(grid[0])
+height = len(grid)
+
+def get(x, y):
+    if x < 0 or x >= width or y < 0 or y >= height:
+        return False
+
+    return grid[y][x]
+
+total = 0
+for y in range(len(grid)):
+    for x in range(len(grid[y])):
+        if get(x, y):
+            count = 0
+
+            for dy in [-1, 0, 1]:
+                for dx in [-1, 0, 1]:
+                    if dy == 0 and dx == 0: #do not check same square
+                        continue
+
+                    if get(x + dx, y + dy):
+                        count += 1
+
+            if count < 4:
+                total += 1
+
+print(total)
